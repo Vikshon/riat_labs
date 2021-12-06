d33d74090afefbef32a96a752bd197daf2301940
KIKIWHYY <yougonnadie123@mail.ru>
2021-12-07 02:26:36 +0400

    seralize complete

--- serialize/serialize.js
+++ serialize/serialize.js
@@ -1,4 +1,7 @@
 const xml_convert = require('xml-js');
+const { XMLParser, XMLBuilder } = require('fast-xml-parser');
+const builder = new XMLBuilder();
+const parser = new XMLParser();
 
 class Input {
     constructor(type = "", obj = {}) {
@@ -10,23 +13,32 @@ class Input {
             this.Muls = parsed.Muls;
         }
         else if (type == "xml") {
-            let parsed = xml_convert.xml2js(obj, {compact: true, nativeType: true});
-            this.K = parsed.Input.K._text;
-            this.Sums = parsed.Input.Sums.decimal.map(el => el._text);
-            this.Muls = parsed.Input.Muls.int.map(el => el._text);
+            let parsed = parser.parse(obj);
+            this.K = parsed.Input.K;
+            this.Sums = parsed.Input.Sums.decimal;
+            this.Muls = parsed.Input.Muls.int;
         }
-    }
-    sayBoo() {
-        console.log("Boo!");
     }
 }
 
 class Output {
-    
+    Deserialize(variable) {
+        this.SumResult = +(variable.Sums.reduce((a, b) => a + b) * variable.K).toFixed(2);
+        this.MulResult = +(variable.Muls.reduce((a, b) => a * b)).toFixed(2);
+        this.SortedInputs = [...variable.Sums, ...variable.Muls].sort();
+        console.log(this.SumResult, this.MulResult, this.SortedInputs);
+
+        if (variable.type == "json") {
+            return JSON.stringify({SumResult: this.SumResult, MulResult: this.MulResult, SortedInputs: this.SortedInputs});
+        }
+        else if (variable.type == "xml") {
+            return builder.build({ Output: { SumResult: this.SumResult, MulResult: this.MulResult, SortedInputs: {decimal: this.SortedInputs } } });
+        }
+    }
 }
 
 const test_json = new Input("json", `{"K":10,"Sums":[1.01,2.02],"Muls":[1,4]}`);
 const test_xml = new Input("xml", `<Input><K>10</K><Sums><decimal>1.01</decimal><decimal>2.02</decimal></Sums><Muls><int>1</int><int>4</int></Muls></Input>`);
 
-const a = new Input("boo");
-console.log(test_xml);
+let out = new Output()
+console.log(out.Deserialize(test_json));
